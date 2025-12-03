db9634bad1eac3ab056b707c4ad194b27384d792
Abhishek Zatakiya <114485563+Zatakiya2510@users.noreply.github.com>
2025-12-03 22:23:29 +0530

    Final

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.post("/verify-otp", verifyOtp); // Step 2: Verify OTP
 router.post("/register", register); // Step 3: Register user
 router.post("/login", login); // Step 4: Login
 router.post("/send-reset-otp", sendOtpForPasswordReset);
-router.post("/verify-rest-otp",verifyResetOtp)
+router.post("/verify-reset-otp",verifyResetOtp)
 router.post("/reset-password", setNewPassword);
 
 export default router;
